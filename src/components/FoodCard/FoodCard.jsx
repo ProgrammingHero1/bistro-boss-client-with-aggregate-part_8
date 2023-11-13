f9a9263ea82ae58b3afa89d9d3cff825668d3fbd
Programming Hero <programming.hero1@gmail.com>
2023-11-12 19:39:14 -0600

    use refetch to update cart after add to cart action

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,8 +2,7 @@ import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
-
-
+import useCart from "../../hooks/useCart";
 
 
 const FoodCard = ({ item }) => {
@@ -12,11 +11,11 @@ const FoodCard = ({ item }) => {
     const navigate = useNavigate();
     const location = useLocation();
     const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
 
-    const handleAddToCart = food => {
+    const handleAddToCart = () => {
         if (user && user.email) {
-            // TODO: send cart item to the database
-            console.log(user.email, food);
+            //send cart item to the database
             const cartItem = {
                 menuId: _id,
                 email: user.email,
@@ -35,6 +34,8 @@ const FoodCard = ({ item }) => {
                             showConfirmButton: false,
                             timer: 1500
                         });
+                        // refetch cart to update the cart items count
+                        refetch();
                     }
 
                 })
@@ -65,7 +66,7 @@ const FoodCard = ({ item }) => {
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
                     <button
-                        onClick={() => handleAddToCart(item)}
+                        onClick={handleAddToCart}
                         className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4"
                     >Add to Cart</button>
                 </div>
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,19 +1,21 @@
+// api, axios (axios secure), tan stack 
+
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
-
-
+import useAuth from "./useAuth";
 
 const useCart = () => {
-    // tan stack query 
     const axiosSecure = useAxiosSecure();
-    const { data: cart = [] } = useQuery({
-        queryKey: ['cart'], 
-        queryFn: async () =>{
-            const res = await axiosSecure.get('/carts')
+    const { user} = useAuth();
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async() => {
+            const res = await axiosSecure.get(`/carts?email=${user.email}`);
             return res.data;
         }
     })
-    return [cart]
+
+    return [cart, refetch]
 };
 
 export default useCart;
