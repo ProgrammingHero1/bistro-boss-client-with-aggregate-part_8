ec93d906362e4f33be924e5afe9c6e491dd60b9c
Programming Hero <programming.hero1@gmail.com>
2023-11-15 21:15:58 -0600

    Create Manage Items admin route and Load all menu items

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
 import AddItems from "../pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
+import ManageItems from "../pages/Dashboard/ManageItems/ManageItems";
 
 
   export const router = createBrowserRouter([
@@ -61,6 +62,10 @@ import AdminRoute from "./AdminRoute";
         {
           path: 'addItems',
           element: <AdminRoute><AddItems></AddItems></AdminRoute>
+        },
+        {
+          path: 'manageItems',
+          element: <AdminRoute><ManageItems></ManageItems></AdminRoute>
         },
         {
           path: 'users',
---
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,78 @@
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+
+
+const ManageItems = () => {
+    const [menu] = useMenu();
+
+    const handleDeleteItem = (item) =>{
+
+    }
+
+    return (
+        <div>
+            <SectionTitle heading="Manage All Items" subHeading="Hurry up"></SectionTitle>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>Image</th>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th>Update</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                menu.map((item, index) => <tr key={item._id}>
+                                    <td>
+                                        {index + 1}
+                                    </td>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item.name}
+                                    </td>
+                                    <td className="text-right">${item.price}</td>
+                                    <td>
+                                        <button
+                                        
+                                        className="btn btn-ghost btn-lg bg-orange-500">
+                                            <FaEdit className="text-white 
+                                        "></FaEdit>
+                                    </button>
+                                    </td>
+                                    <td>
+                                    <button
+                                        onClick={() => handleDeleteItem(item)}
+                                        className="btn btn-ghost btn-lg">
+                                        <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                    </button>
+                                    </td>
+                                </tr>)
+                            }
+                        </tbody>
+                        
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ManageItems;
