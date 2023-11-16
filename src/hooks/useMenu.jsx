14ff0fd86b4cbc2d2330d021d9b1cb57d2dc90f9
Programming Hero <programming.hero1@gmail.com>
2023-11-15 21:46:52 -0600

    Delete menu item and refetch after delete

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-    const [menu, setMenu] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        fetch('http://localhost:5000/menu')
-            .then(res => res.json())
-            .then(data => {
-                setMenu(data);
-                setLoading(false);
-            });
-    }, [])
-    return [menu, loading]
+    const axiosPublic = useAxiosPublic();
+    // const [menu, setMenu] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/menu')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setMenu(data);
+    //             setLoading(false);
+    //         });
+    // }, [])
+
+    const {data: menu = [], isPending: loading, refetch} = useQuery({
+        queryKey: ['menu'], 
+        queryFn: async() =>{
+            const res = await axiosPublic.get('/menu');
+            return res.data;
+        }
+    })
+
+
+    return [menu, loading, refetch]
 }
 
 export default useMenu;
--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,13 +1,42 @@
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+    const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure();
 
     const handleDeleteItem = (item) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then( async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`);
+                // console.log(res.data);
+                if(res.data.deletedCount > 0){
+                    // refetch to update the ui
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${item.name} has been deleted`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
 
+            
+            }
+          });
     }
 
     return (
