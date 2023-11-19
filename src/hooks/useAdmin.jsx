9986b9e19a3ac062c65cc69bf6825e4015ff54ac
Programming Hero <programming.hero1@gmail.com>
2023-11-19 12:58:49 -0600

    Fix Login issue and API race condition

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,11 +4,13 @@ import useAxiosSecure from "./useAxiosSecure";
 
 
 const useAdmin = () => {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
     const axiosSecure = useAxiosSecure();
     const { data: isAdmin, isPending: isAdminLoading } = useQuery({
         queryKey: [user?.email, 'isAdmin'],
+        enabled: !loading,
         queryFn: async () => {
+            console.log('asking or checking is admin', user)
             const res = await axiosSecure.get(`/users/admin/${user.email}`);
             // console.log(res.data);
             return res.data?.admin;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -49,14 +49,16 @@ const AuthProvider = ({ children }) => {
                     .then(res => {
                         if (res.data.token) {
                             localStorage.setItem('access-token', res.data.token);
+                            setLoading(false);
                         }
                     })
             }
             else {
                 // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
                 localStorage.removeItem('access-token');
+                setLoading(false);
             }
-            setLoading(false);
+            
         });
         return () => {
             return unsubscribe();
