5abb0b889425402de60d684bf9b33ff7cfe8cbdb
Programming Hero <programming.hero1@gmail.com>
2023-11-18 21:14:28 -0600

    Send Price and Get client Secret from stripe

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,9 +1,26 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
 
 const CheckoutForm = () => {
+    const [error, setError] = useState('');
+    const [clientSecret, setClientSecret] = useState('')
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price: totalPrice })
+            .then(res => {
+                console.log(res.data.clientSecret);
+                setClientSecret(res.data.clientSecret);
+            })
+
+    }, [axiosSecure, totalPrice])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -23,11 +40,13 @@ const CheckoutForm = () => {
             card
         })
 
-        if(error){
-            console.log('payment error', error)
+        if (error) {
+            console.log('payment error', error);
+            setError(error.message);
         }
-        else{
+        else {
             console.log('payment method', paymentMethod)
+            setError('');
         }
     }
 
@@ -49,9 +68,10 @@ const CheckoutForm = () => {
                     },
                 }}
             />
-            <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
+            <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe || !clientSecret}>
                 Pay
             </button>
+            <p className="text-red-600">{error}</p>
         </form>
     );
 };
