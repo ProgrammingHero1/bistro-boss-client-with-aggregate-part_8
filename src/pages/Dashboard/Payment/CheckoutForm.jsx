bc5d4cfc2449739916f4ce451f47b1c6bdbfa8d9
Programming Hero <programming.hero1@gmail.com>
2023-11-18 21:29:20 -0600

    Use Confirm Card Payment to Process Payment

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,14 +2,17 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 
 const CheckoutForm = () => {
     const [error, setError] = useState('');
     const [clientSecret, setClientSecret] = useState('')
+    const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
     const [cart] = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
 
@@ -48,6 +51,29 @@ const CheckoutForm = () => {
             console.log('payment method', paymentMethod)
             setError('');
         }
+
+        // confirm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+                }
+            }
+        })
+
+        if (confirmError) {
+            console.log('confirm error')
+        }
+        else {
+            console.log('payment intent', paymentIntent)
+            if (paymentIntent.status === 'succeeded') {
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id);
+            }
+        }
+
     }
 
     return (
@@ -72,6 +98,7 @@ const CheckoutForm = () => {
                 Pay
             </button>
             <p className="text-red-600">{error}</p>
+            {transactionId && <p className="text-green-600"> Your transaction id: {transactionId}</p>}
         </form>
     );
 };
--- src/pages/Dashboard/Payment/__payment_steps__.js
+++ src/pages/Dashboard/Payment/__payment_steps__.js
@@ -3,4 +3,9 @@
  * 2. create card element 
  * 3. create stripe account and get publishable key
  * 4. use publishable key and use stripe to get card information and error
+ * 5. create payment intent post on the server. and return client secret. install stripe on the server side and get client secret. make sure you used the payment method types: ['card']
+ * 6. from client side get the client secret and save it. 
+ * 7. use confirm card payment and pass user information, card and client secret
+ * 8. display transaction id
+ * 
 */
